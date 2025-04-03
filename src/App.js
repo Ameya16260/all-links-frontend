cacb43230aac40400a266db1855ec49ee96f97a7
Ameya16260 <134862779+Ameya16260@users.noreply.github.com>
2025-04-04 00:49:41 +0530

    react site to show passwords

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const [links, setlinks] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/allPass")
+      .then(response => response.json())
+      .then(data => {
+        setlinks(data);
+      })
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {links.map(item => (
+        <div className='box'>
+          {item.name && <div className="name">Name: {item.name}</div>}
+          <div className='url'>Url: {item.url}</div>
+          {item.password && <div className="pass">Password: {item.password}</div>}
+          {item.desc && <div className="desc">Description: {item.desc}</div>}
+          <div className='but'>
+            <a href={item.url} target="_blank">
+              Visit Url
+            </a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
